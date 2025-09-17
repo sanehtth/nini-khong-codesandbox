c299a4a6cc8645f21d03a3a8f1a7ff0aec558600
sanehtth <sane.htth@gmail.com>
2025-09-18 00:15:33 +0700

    Delete netlify/functions/functions/admin-logout.js

--- netlify/functions/functions/admin-logout.js
+++
@@ -1,7 +0,0 @@
-﻿exports.handler = async () => {
-  const COOKIE_NAME = process.env.COOKIE_NAME || "nini_admin";
-  const cookie = [
-    `${COOKIE_NAME}=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0`
-  ].join("; ");
-  return { statusCode: 200, headers: { "Set-Cookie": cookie }, body: "OK" };
-};

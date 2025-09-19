7c4f575724311cfe081c1464a3b383fa8132f08f
sanehtth <sane.htth@gmail.com>
2025-09-19 14:24:11 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -58,7 +58,17 @@
       <li>Phát triển bản thân: Bứt phá khỏi những giới hạn của bản thân, tư duy logic và kỹ năng giải quyết vấn đề của bạn sẽ được nâng cao một cách đáng kể.</li>
     </ul>
       <p>Hãy tải <strong>NiNi — Funny</strong> ngay hôm nay và bắt đầu hành trình biến tiếng Anh thành một niềm vui bất tận.</p>`,
-    rules: `<h2>Luật chơi</h2><p>Mỗi mini game có hướng dẫn ngắn ngay trước khi bắt đầu. Chơi vui, công bằng và tôn trọng bạn chơi.</p>`,
+    rules: `<h2>Luật chơi</h2><p>Mỗi mini game có hướng dẫn ngắn ngay trước khi bắt đầu. Chơi vui, công bằng và tôn trọng bạn chơi.</p>
+    <p>Tuy nhiên, mình sẽ tiếc lộ một bí mật nho nhỏ, bạn muốn kiếm được nhiều xu thì hãy tham gia CLUB hoặc tự mình thành lập một CLUB cho riêng mình.</p>
+    <p>Tin mình đi! bạn sẽ thấy điều hấp dẫn ngay thôi! ==>> vào CLB thôi. </p>
+    <p>Điều kiện thành lập CLB:</p>
+    <ul>
+    <li> Bạn phải là thành viên của gia đình Nini bằng cách đăng nhập.</li>
+    <li> Tại thời điểm xin tạo CLUB, xu của bạn >= 400 xu</li>
+    </ul>
+    <p> Nếu không đủ điều kiện trên, không sao cả, bạn có thê xin vào CLUB của các bạn khác!Vẫn được rất nhiều ưu đãi không ngờ nha!</p>
+    <p> Cố lên...con đường thử thách ddang trước mặt bạn...dũng cảm bước đi nào!</p>
+    `,
     forum: `<h2>Diễn đàn</h2><p>Góc để bé khoe thành tích, trao đổi mẹo chơi và đặt câu hỏi.</p>`,
     feedback:`<h2>Góp ý</h2><p>Bạn có ý tưởng trò chơi mới hoặc phát hiện lỗi? Hãy góp ý để NiNi tốt hơn!</p>`
   };
@@ -104,6 +114,7 @@
   // preload ảnh để chuyển mượt
   Object.values(IMAGES).forEach(src => { const i = new Image(); i.src = src; });
 })();
+
 
 
 

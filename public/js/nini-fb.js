71fe2b10548b3c03c52856fdb6cfac3bc718e811
sanehtth <sane.htth@gmail.com>
2025-10-10 13:08:00 +0700

    Update nini-fb.js

--- public/js/nini-fb.js
+++ public/js/nini-fb.js
@@ -16,6 +16,15 @@
   var _ready = false;
   var _subs = []; // onUserChanged subscribers
 
+  // ---- init ----
+const firebaseConfig = window.__NINI_FIREBASE_CONFIG__ || {
+  apiKey: "AIzaSyBdaMS7aI03wHLhi1Md2QDitJFkA61IYUU",
+  authDomain: "nini-8f3d4.firebaseapp.com",
+  projectId: "nini-8f3d4",
+  storageBucket: "nini-8f3d4.firebasestorage.app",
+  messagingSenderId: "991701821645",
+  appId: "1:991701821645:web:fb21c357562c6c801da184",
+};
   // ---------- Utilities ----------
   function noop() {}
   function onceDomReady(cb) {
@@ -166,3 +175,4 @@
   // Init asap (sau DOM để tránh race với config)
   onceDomReady(initIfNeeded);
 })();
+

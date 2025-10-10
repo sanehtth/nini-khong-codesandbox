c04e794e9efea66d22bd3ec07506323236e09013
sanehtth <sane.htth@gmail.com>
2025-10-10 13:26:37 +0700

    Add files via upload

---
+++ public/js/nini-config.js
@@ -0,0 +1,11 @@
+﻿// /public/js/nini-config.js
+
+
+window.__NINI_FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyBdaMS7aI03wHLhi1Md2QDitJFkA61IYUU",
+  authDomain: "nini-8f3d4.firebaseapp.com",
+  projectId: "nini-8f3d4",
+  storageBucket: "nini-8f3d4.firebasestorage.app",
+  messagingSenderId: "991701821645",
+  appId: "1:991701821645:web:fb21c357562c6c801da184",
+};
